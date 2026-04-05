fbc3b613e18b7ca76223d5fd5feb1823e5584679
Nithyashree L <nithyashree03feb2005@gmail.com>
2026-04-05 14:58:51 +0530

    Delete Heart Disease Prediction directory

--- Heart Disease Prediction/src/ecg_features.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from ecg_generator import generate_ecg
-
-def get_ecg():
-    return generate_ecg()
-
-def extract_features(signal):
-    return [np.mean(signal), np.std(signal), np.max(signal), np.min(signal)]
